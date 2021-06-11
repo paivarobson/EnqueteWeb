c3c3e07a7afeaeb967240ab00118ad1b58d27a25
Robson Paiva <paivawork@hotmail.com>
2021-06-11 20:54:23 -0300

    Formulário para enquete e suas funções criado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,5 +15,18 @@ namespace EnqueteWeb.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Responder()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Responder(Resposta resposta)
+        {
+            Repositorio.AdicionarResposta(resposta);
+            return Content("Recebemos sua resposta com sucesso.");
+        }
     }
 }
---
+++ Models/Resposta.cs
@@ -0,0 +1,9 @@
+namespace EnqueteWeb.Models
+{
+    public class Resposta
+    {
+        public string Nome {get; set; }
+        public string Email {get; set; }
+        public bool? Sin { get; set; }
+    }
+}
